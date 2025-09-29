bdbb204df0a546b7b6725ef411564e6e625656b3
jattiphrswan <jattiphrswan49@gmail.com>
2025-09-29 16:50:20 +0530

    Fix HashRouter import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
 import Layout from './Layout.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
@@ -9,6 +7,7 @@ import Contact from './components/Contact/Contact.jsx';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
