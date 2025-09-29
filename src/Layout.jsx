0ca5109dc3fb85e87ca3fd80de45c4cb708d0375
jattiphrswan <jattiphrswan49@gmail.com>
2025-09-29 17:04:10 +0530

    Update main.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,25 +1,16 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function Layout() {
   return (
     <>
-    <Header />
-    <Outlet />
-    <Footer /><HashRouter>
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-    </Route>
-  </Routes>
-</HashRouter>
-
+      <Header />
+      <Outlet /> {/* This renders the page component (Home, About, Contact) */}
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
